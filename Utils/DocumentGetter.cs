4ba4ba75b2143bb98d7ac501ca5bc7602bfda66d
andriusandriusandrius <and.zurba@gmail.com>
2025-11-27 00:05:44 +0200

    add document store from disk to memory

---
+++ Utils/DocumentGetter.cs
@@ -0,0 +1,17 @@
+namespace search_engine.Utils
+{
+    public static class DocumentGetter
+    {
+        public static Dictionary<int, string> GetDocuments()
+        {
+            Dictionary<int,string> documents = new();
+            int id = 0;
+            foreach(var path in Directory.GetFiles("Documents"))
+            {
+                documents[id] = File.ReadAllText(path);
+                id++;
+            }
+            return documents;
+        }
+    }
+}
