1fb59e5f7966756df754c69d51ce5ba4e45f3d28
andriusandriusandrius <and.zurba@gmail.com>
2025-11-28 15:41:12 +0200

    add tf-idf ranking, create documentFiles

--- Models/InvertedIndex.cs
+++ Models/InvertedIndex.cs
@@ -3,6 +3,7 @@ namespace search_engine.Models
     public class InvertedIndex
     {
         public Dictionary<string, List<Posting>> Index { get; } = new();
-        public Dictionary<int, string> Documents { get; set; } = new();
+        public Dictionary<int, DocumentFile> Documents { get; } = new();
+        public int TotalDocuments { get; set; } = 0;
     }
 }
--- Utils/DocumentGetter.cs
+++ Utils/DocumentGetter.cs
@@ -1,14 +1,19 @@
+using search_engine.Models;
+
 namespace search_engine.Utils
 {
     public static class DocumentGetter
     {
-        public static Dictionary<int, string> GetDocuments()
+        public static Dictionary<int, DocumentFile> GetDocuments()
         {
-            Dictionary<int, string> documents = new();
-            int id = 0;
+            Dictionary<int, DocumentFile> documents = new();
+            int id = 1;
             foreach (var path in Directory.GetFiles("Documents"))
             {
-                documents[id] = File.ReadAllText(path);
+                documents[id] = new DocumentFile(
+                     Path.GetFileNameWithoutExtension(path),
+                    File.ReadAllText(path)
+                );
                 id++;
             }
             return documents;
