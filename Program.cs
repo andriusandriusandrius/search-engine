83eee639575b10b733a5745fde1011cf2061d433
andriusandriusandrius <and.zurba@gmail.com>
2025-12-01 16:02:42 +0200

    add program run example

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using search_engine.Engine;
+using search_engine.Utils;
+SearchEngine searchEngine = new SearchEngine();
+
+
+var documents = DocumentGetter.GetDocuments();
+
+foreach (var document in documents)
+{
+    searchEngine.IndexDocument(document.Value);
+}
+var ats = searchEngine.Search("instantly and space");
+foreach (var at in ats)
+{
+    Console.WriteLine($" Document: {at.Document.Title}, Score {at.Score}");
+}
