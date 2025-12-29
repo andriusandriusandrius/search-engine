fd19ce5a0c99348a4ca160767d57f7dbfdcb762e
andriusandriusandrius <and.zurba@gmail.com>
2025-12-29 14:19:21 +0200

    change posting to compare by id in hashsets

--- Models/Tokens/PhraseToken.cs
+++ Models/Tokens/PhraseToken.cs
@@ -1,7 +1,17 @@
+using search_engine.Models.Nodes;
+
 namespace search_engine.Models.Tokens
 {
     public class PhraseToken : Token, INodifiable
     {
-
+        public List<string> Terms { get; }
+        public PhraseToken(List<string> terms, int position) : base(string.Join(" ", terms), position)
+        {
+            Terms = terms;
+        }
+        public void Nodify(Stack<IQueryNode> output)
+        {
+            output.Push(new PhraseNode(Terms));
+        }
     }
 }
