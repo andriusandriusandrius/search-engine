5052317b5d9769f6ca8575541c06c51547d0bbe7
andriusandriusandrius <and.zurba@gmail.com>
2025-12-30 10:31:05 +0200

    add the meat of phrase logic

--- Models/Posting.cs
+++ Models/Posting.cs
@@ -1,4 +1,23 @@
 namespace search_engine.Models
 {
-    public record Posting(int DocId, List<int> Positions);
+    public class Posting
+    {
+        public int DocId { get; }
+        public List<int> Positions { get; }
+
+        public Posting(int docId, List<int> positions)
+        {
+            DocId = docId;
+            Positions = positions;
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is Posting other && other.DocId == DocId;
+        }
+
+        public override int GetHashCode()
+        {
+            return DocId;
+        }
+    }
 }
